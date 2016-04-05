36d46b5f48e51d3d4da3db80ae2307f401a64273
Chameleon87 <jessehodge1987@gmail.com>
2016-04-05 18:01:58 -0500

    Making changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,6 @@ import {Component} from 'angular2/core';
     <div>
     <h3>{{contact.firstName}} {{contact.lastName}}</h3>
     <p>Email address: {{contact.email}}</p>
-    <p>Phone Number: {{contact.phone}}</p>
 </div>
 `,
 })
