2ea2ac4d4927578a253e8cb6c614a8b0b403024e
Chameleon87 <jessehodge1987@gmail.com>
2016-04-10 17:13:17 -0500

    Adding a home page component

--- app/app.ts
+++ app/app.ts
@@ -1,19 +1,10 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
-//import { Routes, APP_ROUTES } from './routes.config';
-import {HTTP_PROVIDERS} from 'angular2/http';
-import { Home } from './home/Home';
+import { Home } from './app/home/Home';
 
 @Component({
     selector: 'app',
-    templateUrl: './home/home.html',
-    directives: [ROUTER_DIRECTIVES],
-    providers: [HTTP_PROVIDERS, ROUTER_PROVIDERS]
+    templateUrl: './app/home/home.html',
 })
-
-@RouteConfig([
-    { path: '/', name: 'Home', component: Home, useAsDefault: true }    
-])
 
 export class App { 
     public contact = {firstName:"Jesse", lastName:"Hodge", email:"Jessehodge1987@gmail.com", phone:"512-412-9170"};
--- app/routes.config.ts
+++
@@ -1,8 +0,0 @@
-import { Home } from './home/Home';
-import { Route, Router } from 'angular2/router';
-
-export var Routes = {
-    home: new Route({ path: '/', name: 'Home', component: Home })
-};
-
-export const APP_ROUTES = Object.keys(Routes).map(r => Routes[r]);
